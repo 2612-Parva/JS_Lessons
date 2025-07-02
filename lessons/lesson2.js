5c6d9fb90ff7e645c03f9f393b9137ede5b3d78c
Parva Mineshbhai Patel <pr548271@dal.ca>
2025-07-02 14:29:55 -0300

    Add lesson2

---
+++ lessons/lesson2.js
@@ -0,0 +1,12 @@
+//Concatenation and Interpolation
+
+//Concatenation
+let price=60;
+let itemName="Cup";
+let messagetoPrint="The price of " + itemName + " is " + price + " dollars";
+console.log(messagetoPrint);
+
+//Interpolation
+
+let messagetoPrint2 = `The price of ${itemName} is ${price} dollars`;
+console.log(messagetoPrint2);
