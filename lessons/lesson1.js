e02c39ba64cbe673bb5da5181f6ca6117ac08ac0
Parva Mineshbhai Patel <pr548271@dal.ca>
2025-07-02 14:26:44 -0300

    Add lesson1

---
+++ lessons/lesson1.js
@@ -0,0 +1,31 @@
+1.//Hello World
+
+console.log("Hello World");
+
+2.// Variables
+
+var firstname= "Parva";
+let lastname= "Patel";
+
+console.log(lastname);
+
+var age, dateofbirth, gender
+
+age= "20";
+gender = "Male";
+console.log(age);
+
+age = "21";
+console.log(age);
+
+//constants
+
+const occupations = "Student";
+console.log(occupations);
+
+// Data Types
+var middlename="Mineshbhai"; //string
+var ageofbrother= 18;//number
+var ishemarried= false; //boolean
+var yearsInMarriage=null; //null
+var numberofcars=undefined; //undefined
