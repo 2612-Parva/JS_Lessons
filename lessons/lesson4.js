bf237c202c3ae49bf7ced67f0770978b05f3b5a9
Parva Mineshbhai Patel <pr548271@dal.ca>
2025-07-14 23:33:26 -0300

    Add Lesson4

---
+++ lessons/lesson4.js
@@ -0,0 +1,23 @@
+// Relational or comparison operators
+
+// >= more than or equal to
+// <= less than or equal to
+// > more than
+// < less than
+
+var a = 10>20;
+var b = 10<20;
+var c = 10>=20;
+var d = 10<=20;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// equalit operators
+
+var e = 10;
+var f = '10';
+console.log(e==10); // lose comparison
+console.log(f===10); // strict comparison
