0195ac52026b97c82c8df4858bd0cb630aadb1a8
Parva Mineshbhai Patel <pr548271@dal.ca>
2025-07-16 23:45:42 -0300

    Add lesson10

---
+++ lessons/lesson10.ts
@@ -0,0 +1,11 @@
+var customerFirstName = "Parva";
+var customerLastName = "Patel";
+var age = 25;
+
+type Customer = {firstname:string, lastname:string, age:number};
+
+var CustomerDetails: Customer={
+    firstname: "Parva",
+    lastname:"Patel",
+    age:25
+}
